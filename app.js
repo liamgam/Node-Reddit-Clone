d5f1b1cd5a0716c906ba4455fdf5691cdf59ec81
liamgam <liam.liamgamble@gmail.com>
2019-05-30 13:27:52 -0700

    added comments for middleware, and routes

--- app.js
+++ app.js
@@ -2,13 +2,26 @@
 const express = require('express'),
       exphbs = require('express-handlebars'),
       app = express(),
-      users = require('./users');
+      users = require('./users'),
+      postsRouter = require('./controllers/posts');
+
+const bodyparser = require('body-parser'),
+      expressValidator = require('express-validator');
+
+
+// - Initial Setup
 
 app.engine('handlebars', exphbs({ defaultlayout: 'main'}));
 app.set('view engine', 'handlebars')
-
 app.set('port', process.env.PORT || 3000);
 
+// - Middleware
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(expressValidator())
+
+// - Routes
 app.get('/', (req,res) => {
    res.render('home')
 })
@@ -17,9 +30,8 @@ app.get('/users', (req, res) => {
   response.json(users)
 })
 
-app.get('/posts/new',(req,res) => {
-   res.render('posts-new')
-})
+app.use('/posts', postsRouter)
+
 
 
 app.listen(3000,() => {
