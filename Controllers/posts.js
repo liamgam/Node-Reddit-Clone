b821a21bdafa7ef8eee8887834cc9dac5a8feb6d
liamgam <liam.liamgamble@gmail.com>
2019-05-30 17:24:10 -0700

    created new post saves to database

--- Controllers/posts.js
+++ Controllers/posts.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const Post = require('../models/post');
+
 router.post('/',(req,res) => {
-   console.log(req.body)
+   // console.log(req.body)
+   const post = new Post(req.body);
+
+   post.save((err, post) => {
+   	return res.redirect('/');
+   })
  })
 
 router.get('/new',(req,res) => {
