18d8bf4845a9117a905df6bdbe13a7c8359b780e
liamgam <liam.liamgamble@gmail.com>
2019-06-03 15:17:46 -0700

    fixed crashing from setting headers twice by removing the render from the save method

--- Controllers/posts.js
+++ Controllers/posts.js
@@ -5,16 +5,22 @@ const router = express.Router();
 const Post = require('../models/post');
 
 router.post('/',(req,res) => {
-   // console.log(req.body)
    const post = new Post(req.body);
 
-   post.save((err, post) => {
-   	return res.redirect('/');
+   post.save()
+
+   Post.find({})
+   .then(posts => {
+   	res.render('posts-index', {posts});
    })
- })
+   .catch(err => {
+   	console.log(err.message)
+   })
+})
 
 router.get('/new',(req,res) => {
    res.render('posts-new')
 })
+
 
 module.exports = router;
