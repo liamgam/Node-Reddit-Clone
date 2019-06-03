8d3af1498907264762a9cdfbef17df59a90f15af
liamgam <liam.liamgamble@gmail.com>
2019-06-03 16:36:52 -0700

    added app.address for mocha testing

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ app.get('/users', (req, res) => {
 app.use('/posts', postsRouter)
 
 
+// - Set up Server
 
 app.listen(3000,() => {
   console.log('Express server listening at port 3000')
 })
+
+module.exports = app
